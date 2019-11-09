08b370f72e93aa99b61d50c3d8a9aa7f8283c264
osamtimizer <eaten.jb@gmail.com>
2019-11-09 18:24:41 +0900

    impl minimum observables

--- Sources/RxSwiftExecutable/main.swift
+++ Sources/RxSwiftExecutable/main.swift
@@ -1,4 +1,25 @@
 import RxSwift
 import RxCocoa
+import Foundation
 
-print("hello world")
+print("main start")
+
+let firstObservable = PublishRelay<String?>()
+let secondObservable = BehaviorRelay<Int>(value: 100)
+
+firstObservable.flatMap(Observable.from(optional:))
+  .subscribe(onNext: { str in
+    print(str)
+  })
+
+secondObservable.map { int in int * 200 }
+  .subscribe(onNext: { int in
+    print(int)
+  })
+
+firstObservable.accept("before nil")
+firstObservable.accept(nil)
+firstObservable.accept("after nil")
+secondObservable.accept(5)
+
+print("main end")
